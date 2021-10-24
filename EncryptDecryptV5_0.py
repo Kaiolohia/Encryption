07af54c765d0835cecbf8c4c137da6a6bd3e1a3b
TruxSux <kaithomasdean@outlook.com>
2021-10-24 04:43:42 -1000

    Finished seeded encrypt/decrypt

--- EncryptDecryptV5_0.py
+++ EncryptDecryptV5_0.py
@@ -15,17 +15,27 @@ import base62
 class EncryptDecrypt():
     '''
     EncryptDecrypt takes in 3 optional seeds, seed_1 is your "main" seed
-    seed_2 is your alphabetical seed, this takes in a list of LOWERCASE alphabet
+    seed_2 is your alphabetical seed, this takes in a list of UPPER AND LOWERCASE letters from the english alphabet
     in any order ["x", "s", "t"...]
     seed_3 is your numerical seed, this takes a list of numbers 0-9 in any order [5,1,2...]
     '''
+
     def __init__(self, 
-    seed_1:int = 465123789, 
-    seed_2:list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-    seed_3:list = [0,1,2,3,4,5,6,7,8,9]):
-        self.seed_1 = seed_1
-        self.seed_2 = seed_2
-        self.seed_3 = seed_3
+    public_seed:str = "123456789", 
+    private_seed:str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ"):
+        self.public_seed = base62.decode(public_seed) if public_seed != "123456789" else "123456789"
+        self.private_seed = "".join(from_number(str(private_seed))) if private_seed != "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ" else "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ"
+        destructured_seed = self.destructure_priv_seed()
+        self.seed_2 = list(destructured_seed[1])
+        self.seed_3 = list(destructured_seed[0])
+
+    def destructure_priv_seed(self):
+        destructure = []
+        destructure.append(self.private_seed[0:10])
+        destructure.append(self.private_seed[10:61])
+        destructure.append(self.private_seed[61:])
+        return destructure
+        
 
     def encrypt(self, msg): #unseeded encryption, leaves tools to decrypt in the message
         msg = to_number(msg)
@@ -37,9 +47,21 @@ class EncryptDecrypt():
             msg[i] = str(int(msg[i]) + ord(char1) + ord(char2)).zfill(3)
             letter_map.insert(i,str(ord(char1)).zfill(3))
             letter_map.append(str(ord(char2)).zfill(3))
-        msg_len = len(msg[:])
         msg[0] = str(int(msg[0]) + 200)
         return base62.encode(int(''.join(numbers_encrypt("".join(msg + letter_map)))))
+    
+    def encrypt_seeded(self, msg):
+        msg = to_number(msg)
+        letter_map = []
+        random.seed(self.public_seed)
+        choices = self.seed_2[:]
+        random.shuffle(choices)
+        for i in range(len(msg)):
+            char1 = random.choice(choices)
+            char2 = random.choice(choices)
+            msg[i] = str(int(msg[i]) + ord(char1) + ord(char2)).zfill(3)
+        msg[0] = str(int(msg[0]) + 200)
+        return base62.encode(int(''.join(numbers_encrypt("".join(msg)))))
 
     def decrypt(self, msg):
         """
@@ -56,6 +78,20 @@ class EncryptDecrypt():
         for i in range(len(des_msg)):
             des_msg[i] = chr(int(des_msg[i]) - int(letter_map_1[i]) - int(letter_map_2[i]))
         return "".join(des_msg)
+
+    def decrypt_seeded(self, msg):
+        msg = ascii_chunk(numbers_decrypt(list(str(base62.decode(msg)))))
+        msg[0] = str(int(msg[0]) - 200).zfill(3)
+        #decrypting
+        random.seed(self.public_seed)
+        choices = self.seed_2[:]
+        random.shuffle(choices)
+        for i in range(len(msg)):
+            char1 = random.choice(choices)
+            char2 = random.choice(choices)
+            msg[i] = chr(int(msg[i]) - int(ord(char1)) - int(ord(char2)))
+        return "".join(msg)
+
 def numbers_encrypt(msg, seed = [0,1,2,3,4,5,6,7,8,9]):
     """
     Encrypts numbers by pulling their index from scramble array
@@ -102,17 +138,17 @@ def to_number(msg):
         nums.append(str(ord(char)).zfill(3))
     return nums
 
-# def from_number(msg):
-#     nums = []
-#     chunk = ''
-#     for i in range(1, len(msg) + 1):
-#         if i%3 == 0:
-#             chunk += msg[i-1]
-#             nums.append(str(chr(int(chunk))))
-#             chunk = ''
-#         else:
-#             chunk += msg[i-1]
-#     return nums
+def from_number(msg):
+  nums = []
+  chunk = ''
+  for i in range(1, len(msg) + 1):
+    if i%3 == 0:
+      chunk += msg[i-1]
+      nums.append(str(chr(int(chunk))))
+      chunk = ''
+    else:
+      chunk += msg[i-1]
+  return nums
 
 def ascii_chunk(input:str):
     chunks = []
@@ -120,6 +156,23 @@ def ascii_chunk(input:str):
         chunks.append("".join(input[i:i+3]))
     return chunks
 
-ed = EncryptDecrypt()
+def seed_gen_pub():
+    seed = random.randint(1,10**6021)
+    return base62.encode(seed)
 
-print(ed.encrypt("sample"))
+def seed_gen_priv():
+    random.seed()
+    nums = ["0","1","2","3","4","5","6","7","8","9"]
+    letters = list(string.ascii_letters)
+    seed = ""
+    
+    random.shuffle(nums)
+    seed += "".join(nums)
+
+    random.shuffle(letters)
+    seed += "".join(letters)
+
+    random.shuffle(nums)
+    random.shuffle(letters)
+    seed += "".join(nums) + "".join(letters)
+    return "".join(to_number(seed))
