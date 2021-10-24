20badc0251ae364b6a9396ce1bcfb91de7da5617
TruxSux <kaithomasdean@outlook.com>
2021-10-24 03:43:59 -1000

    Deleted unnessacary files

--- EncryptBuster.py
+++
@@ -1,7 +0,0 @@
-import EncryptDecryptV4_C
-ED = EncryptDecryptV4_C.EncryptDecrypt()
-
-def main():
-    ED.encrypt("this is a message with around 100 characters to test through my method and then again through hyper fine, I hope that this is of sufficient length")
-    
-main()
