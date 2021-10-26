d3d6c9eb2f7e1baa0988bbf8cfd12be5e9a2bc17
Kai "Trux <59106905+TruxSux@users.noreply.github.com>
2021-10-25 18:28:34 -1000

    Added comments

--- EncryptDecryptV5_0.py
+++ EncryptDecryptV5_0.py
@@ -11,6 +11,9 @@ class EncryptDecrypt():
     def __init__(self, 
     public_seed:str = "123456789", 
     private_seed:str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ"):
+        """
+        Takes in seeds and destructures them
+        """
         self.public_seed = base62.decode(public_seed) if public_seed != "123456789" else "123456789"
         self.private_seed = "".join(from_number(str(private_seed))) if private_seed != "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ" else "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ"
         destructured_seed = self.destructure_priv_seed()
@@ -19,6 +22,9 @@ class EncryptDecrypt():
         self.s_b62 = base62.seeded(destructured_seed[2])
 
     def destructure_priv_seed(self):
+        """
+        The private seed comes in as one big key with three sub keys
+        """
         destructure = []
         destructure.append(self.private_seed[0:10])
         destructure.append(self.private_seed[10:61])
@@ -27,9 +33,17 @@ class EncryptDecrypt():
         
 
     def encrypt(self, msg): #unseeded encryption, leaves tools to decrypt in the message
+        """
+        This encrypt method takes our message and adds to its ascii value by two other
+        chars that is randomly picked, our message is now a long int, so we run it through my own number
+        encryption method down below. Lastly we send that int through custom base62 for both storage
+        friendly ness and also another layer of security.
+        """
         msg = to_number(msg)
         letter_map = []
         char_choices = string.ascii_letters
+        #For each charecter in our message we convert it into an ascii int then add its value
+        #by two other chars in the form as ints.
         for i in range(len(msg)):
             char1 = random.choice(char_choices)
             char2 = random.choice(char_choices)
@@ -40,6 +54,13 @@ class EncryptDecrypt():
         return base62.encode(int(''.join(numbers_encrypt("".join(msg + letter_map)))))
     
     def encrypt_seeded(self, msg):
+        """
+        Just like the unseeded version, we do similar things in the seeded version
+        But the difference here is that there isnt a clue left in the final message
+        that will lead to the decryption of the message without seeds.
+        Instead of pure random-ness it is seeded before hand. We also seed the base62 and
+        the numbers encryption.
+        """
         msg = to_number(msg)
         letter_map = []
         random.seed(self.public_seed)
@@ -54,7 +75,8 @@ class EncryptDecrypt():
 
     def decrypt(self, msg):
         """
-        Decrypts unseeded encryptions
+        Reads the clues left in the message for how much to adjust the message by
+        the encrypted method. Then we do the steps in reverse.
         """
         msg = ascii_chunk(numbers_decrypt(list(str(base62.decode(msg)))))
         msg[0] = str(int(msg[0]) - 200).zfill(3)
@@ -69,6 +91,9 @@ class EncryptDecrypt():
         return "".join(des_msg)
 
     def decrypt_seeded(self, msg):
+        """
+        This method does the same as the normal decrypt but we add our keys.
+        """
         msg = ascii_chunk(numbers_decrypt(list(str(self.s_b62.decode(msg)))))
         msg[0] = str(int(msg[0]) - 200).zfill(3)
         #decrypting
@@ -122,12 +147,18 @@ def numbers_decrypt(msg:list, seed = [0,1,2,3,4,5,6,7,8,9]):
     return new_msg
 
 def to_number(msg):
+    """
+    Converts a long message into a list of ascii ints
+    """
     nums = []
     for char in msg:
         nums.append(str(ord(char)).zfill(3))
     return nums
 
 def from_number(msg):
+    """
+    Converts an INT of ascii chars from int form to char form.
+    """
   nums = []
   chunk = ''
   for i in range(1, len(msg) + 1):
@@ -140,16 +171,26 @@ def from_number(msg):
   return nums
 
 def ascii_chunk(input:str):
+    """
+    Ascii int's are three digits long
+    This function converts a long int into chunks of three
+    """
     chunks = []
     for i in range(0, len(input), 3):
         chunks.append("".join(input[i:i+3]))
     return chunks
 
 def seed_gen_pub():
+    """
+    Generates a seed that fits the public key format
+    """
     seed = random.randint(1,10**6021)
     return base62.encode(seed)
 
 def seed_gen_priv():
+    """
+    Generates a public seed that fits the format of the private key
+    """
     random.seed()
     nums = ["0","1","2","3","4","5","6","7","8","9"]
     letters = list(string.ascii_letters)
@@ -165,5 +206,3 @@ def seed_gen_priv():
     random.shuffle(letters)
     seed += "".join(nums) + "".join(letters)
     return "".join(to_number(seed))
-
-ed = EncryptDecrypt(private_seed = "049055053054050052048056051057068115082088086067074077069101117116120076111114065113080105081097110100073109107083118079087104098072099078122070112085071066108089075106121090084103119102056057053052055050048054049051105080081106066084097100112089077098109122087101120065075104085099102118076108074071088082121070069079067103117114111068119078086113083090115073116107072110")
